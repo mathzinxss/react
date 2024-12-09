dac874928921b05bd94479aa4ad30121d93fe9c3
mathznxs.dev <math983839179@gmail.com>
2024-12-09 14:41:40 -0300

    1

--- testandoRotas/src/pages/Home.jsx
+++ testandoRotas/src/pages/Home.jsx
@@ -5,6 +5,10 @@ function Home() {
         <div>
             <h1>Home</h1>
             <Menu />
+            <div>
+                <h2>Testando</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quisquam quibusdam cum id magnam iusto deserunt qui unde rerum voluptatem dolorem eligendi porro, recusandae perspiciatis facere, sed dolore similique mollitia.</p>
+            </div>
         </div>
     )
 }
