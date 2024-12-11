478a17924b235d92fa18552e6502f8f38b042fe2
mathznxs.dev <math983839179@gmail.com>
2024-12-11 11:53:27 -0300

    1

---
+++ Projetos/Loja Virtual-React/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  return (
+      <div>
+        {/* TODO Barra de navegação superior */}
+        <div>
+            <div>
+                <div>
+                    <a href="#">
+                        <img src={Logo} alt="logo" />    
+                    </a>                
+                </div>
+            </div>
+        </div>
+        {/* TODO Barra de navegação inferior */}
+        <div></div>
+    </div>
+  )
+}
+
+export default Navbar
