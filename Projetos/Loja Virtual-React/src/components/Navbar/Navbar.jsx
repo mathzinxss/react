785d4e2da60bfb38bc2a686754d7c63be5cabdb6
mathznxs.dev <math983839179@gmail.com>
2024-12-11 23:13:32 -0300

    1

--- Projetos/Loja Virtual-React/src/components/Navbar/Navbar.jsx
+++ Projetos/Loja Virtual-React/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import logo from "../../assets/logo.png";
+import { IoMdSearch } from "react-icons/io"
 
 const Navbar = () => {
   return (
-      <div>
+      <div className='shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         {/* TODO Barra de navegação superior */}
-        <div>
-            <div>
+        <div className='bg-primary/40 py-2'>
+            <div className='container flex justify-between items-center '>
                 <div>
-                    <a href="#">
-                        <img src={Logo} alt="logo" />    
+                    <a href="#" className='font-bold text-2xl sm:text-3xl flex gap-2'>
+                        <img src={logo} alt="logo" className='w-10 uppercase' /> Eternize
                     </a>                
                 </div>
+                {/* TODO Barra de pesquisa*/}
+                <div>
+                    <div className='relative group hidden sm:block'>
+                        <input type="text" placeholder="search" className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border-gray-300 px-3 py-0.5 focus:outline-none focus:border-1 focus:border-primary' />
+
+                        <IoMdSearch 
+                        className='text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3' />
+                    </div>
+                </div>
+                {/*TODO botão de pedido */}
             </div>
         </div>
         {/* TODO Barra de navegação inferior */}
--- Projetos/Loja Virtual-React/tailwind.config.js
+++ Projetos/Loja Virtual-React/tailwind.config.js
@@ -4,8 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ], 
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#b0eef7", 
+        secondary: "#82ebfa"
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        }
+      }
+    },
   },
   plugins: [],
 }
