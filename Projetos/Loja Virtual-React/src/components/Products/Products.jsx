969a3db3aadc938f7bd12b5e6c64138f5bfc0a62
mathznxs.dev <math983839179@gmail.com>
2024-12-20 17:40:50 -0300

    1

---
+++ Projetos/Loja Virtual-React/src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <div className='mt-14 mb-12'>
+            <div className='container'>
+                {/* Seção de cabeçalho */}
+                <div className='text-center mb-10 max-w-[600px] mx-auto'>
+                    <p className='text-sm text-secondary'>Produtos mais vendidos para você</p>
+                    <h1 className='text-3xl font-bold'>Produtos</h1>
+                    <p className='text-xs text-gray-400'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat earum deserunt quasi! Nam, tenetur exercitationem magnam voluptate libero eum commodi quisquam accusamus consequuntur recusandae facere. Quos dignissimos explicabo quia quasi!</p>
+                </div>
+                {/* Seção do corpo  */}
+
+                <div>
+                    <div className='grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 place-items-center gap-5'>
+                        {/* Seção de cartão */}
+                            
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Products
